8cf45aeda1462b651c1e739949bb81f9a39733ec
eslam_khalifa <khalifaeslam754@gmail.com>
2025-07-14 18:13:12 +0300

    add pagination feature

--- NoteCLIPlus/Commands/ListNotesCommand.cs
+++ NoteCLIPlus/Commands/ListNotesCommand.cs
@@ -1,3 +1,5 @@
+using NoteCLIPlus.Helpers;
+
 namespace NoteCLIPlus.Commands
 {
     public class ListNotesCommand : ICommand
@@ -17,12 +19,24 @@ namespace NoteCLIPlus.Commands
                 Console.WriteLine("No notes found. Please add a note first.");
                 return;
             }
+            Console.Write($"Choose a page number (default is 1): ");
+            string? pageInput = Console.ReadLine();
+            int pageNumber = 0;
+            if (string.IsNullOrWhiteSpace(pageInput) || !int.TryParse(pageInput, out pageNumber) || pageNumber < 1)
+            {
+                Console.WriteLine($"Invalid input. Taking default page number = 1...");
+                Thread.Sleep(1000);
+            }
+            PaginationOptions.PageNumber = (pageNumber != 0) ? pageNumber : 1;
+            var startIndex = (PaginationOptions.PageNumber - 1) * PaginationOptions.PageSize;
+            int lastIndex = (notes.Length - startIndex < PaginationOptions.PageSize)? notes.Length - 1 : startIndex + PaginationOptions.PageSize - 1;
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Listing all notes: ");
-            Console.WriteLine("---------------------");
+            Console.WriteLine($"Listing all notes in a page {PaginationOptions.PageNumber}");
+            Console.WriteLine($"(Notes {startIndex + 1} to {lastIndex + 1}) of {notes.Length} notes");
+            Console.WriteLine("-----------------------------");
             Thread.Sleep(1000);
             Console.ForegroundColor = ConsoleColor.White;
-            for (int i = 0; i < notes.Length; i++)
+            for (int i = startIndex; i <= lastIndex; i++)
             {
                 if (!string.IsNullOrWhiteSpace(notes[i]))
                 {
---
+++ NoteCLIPlus/Helpers/PaginationOptions.cs
@@ -0,0 +1,8 @@
+namespace NoteCLIPlus.Helpers
+{
+    public class PaginationOptions
+    {
+        public static int PageNumber { get; set; } = 1;
+        public static int PageSize { get; set; } = 5;
+    }
+}
